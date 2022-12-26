5844d8c078c19f911ced0ee4cc063a63992d7b0a
ZGURemixer <zonagamerultra@outlook.com>
2022-12-26 13:09:13 -0400

    Broke the code entirely, lol.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import Producto from './Componentes/producto';
 import './App.css';
+import {Container, Row} from 'reactstrap';
 
 function App() {
   return (
-    <div>
-      <Producto/>
-    </div>
+      {/* Plantilla para producto disponible en producto.js */}
+        <Container>
+          <Row>
+              <Producto 
+                titulo="Doom Eternal PS4" 
+                imagen="https://www.weplay.cl/pub/media/catalog/product/cache/d406ac87fd7d160c9527a5559b847e93/p/s/ps4_doom-eternal.jpg" 
+                descripcion="Los ejércitos del infierno han invadido la Tierra. Ponte en la piel del Slayer en una épica campaña para un jugador y cruza dimensiones para detener la destrucción definitiva de la humanidad." 
+                precio="666"
+              />
+              <Producto
+                  titulo="Nintendo Switch"
+                  imagen="https://images-na.ssl-images-amazon.com/images/I/71ivrWiYkLL._AC_.jpg"
+                  descripcion="Consola Nintendo Switch Black"
+                  precio="229.000"
+              />
+              <Producto
+                titulo="Consola SONY PS4"
+                imagen="https://estaticos.elperiodico.com/resources/jpg/9/8/playstation-sony-1370947347089.jpg"
+                descripcion="Consola PS4 SLIM 500GB "
+                precio="199.990"
+              />
+              <Producto
+                titulo="Consola XBOX ONE"
+                imagen="https://compass-ssl.xbox.com/assets/dd/2e/dd2ef163-a130-403a-a394-a5b11c1533c8.jpg?n=Xbox-Family_Image-0_X1S-1111_496x279.jpg"
+                descripcion="Consola Xbox One solo juegos digitales "
+                precio="229.000"
+              />
+              <Producto
+                titulo="Consola New Nintendo 2DS"
+                imagen="https://http2.mlstatic.com/nintendo-2ds-xl-black-and-turquoise-nuevo-D_NQ_NP_615117-MLM27995821063_082018-F.jpg"
+                descripcion="2DS XL Black Turqoise"
+                precio="129.990"
+              />
+              <Producto
+                titulo="Big Chungus PS4"
+                imagen="https://static.wikia.nocookie.net/big-chungus/images/7/7a/AB851D95-0AA6-496D-83FC-6616222C8F01.jpeg"
+                descripcion="El videojuego más del mundo"
+                precio="999.999.999"
+              />
+          </Row>
+        </Container>
   );
 }
 
--- src/Componentes/producto.js
+++ src/Componentes/producto.js
@@ -1,13 +1,56 @@
-// Importar bibliotecas
+// -- Código Card Normal --
+
+// import React from 'react';
+// import {Card} from 'reactstrap';
+// function Producto(){
+//     return(
+//         <Card>
+//             ¡Aprendiendo a parecer un imbécil!
+//         </Card>
+//     );
+// }
+// export default Producto
+
+// -- Código Card Columna --
+
+// import React from 'react';
+// import {Col, Card} from 'reactstrap';
+// function Producto(){
+//     return(
+//         <Col sm="4">    
+//             <Card>
+//                 ¡Aprendiendo a parecer un imbécil!
+//             </Card>
+//         </Col> 
+//     );
+// }
+
+// export default Producto
 
 import React from 'react';
-import {Card} from 'reactstrap';
+import {Button, CardText, CardSubtitle, CardTitle, CardBody, CardImg, Col, Card} from 'reactstrap';
+import './producto.css';
 
-function Producto(){
+function Producto(props){
     return(
-        <Card>
-            ¡Aprendiendo a parecer un imbécil!
-        </Card>
+        <Col sm="4">    
+            <Card className="Card" body outline color="primary">
+                <CardImg src={props.imagen}></CardImg>
+                <CardBody>
+                    <CardTitle>{props.titulo}</CardTitle>
+                    <CardSubtitle><b>Valor: </b>{props.precio}</CardSubtitle>
+                    <CardText>
+                        {props.descripcion}
+                    </CardText>
+                    <Button className="Button">
+                        Ver ficha
+                    </Button>
+                    <Button className="Button">
+                        Comprar
+                    </Button>
+                </CardBody>
+            </Card>
+        </Col> 
     );
 }
 
