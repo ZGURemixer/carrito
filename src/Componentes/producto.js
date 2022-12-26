46a7f9150a6926c869423b2e445b538d86b3be9e
ZGURemixer <zonagamerultra@outlook.com>
2022-12-26 11:54:53 -0400

    Added a component!

--- src/Componentes/producto.js
+++ src/Componentes/producto.js
@@ -1,1 +1,14 @@
+// Importar bibliotecas
+
 import React from 'react';
+import {Card} from 'reactstrap';
+
+function Producto(){
+    return(
+        <Card>
+            ¡Aprendiendo a parecer un imbécil!
+        </Card>
+    );
+}
+
+export default Producto
